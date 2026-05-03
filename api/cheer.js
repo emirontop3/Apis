dd3066de5ad4b5fbb0b5074764dd8a86f7327e3f
emirontop3 <ozl.kpz@gmail.com>
2026-05-03 22:37:21 +0300

    Update cheer.js

--- api/cheer.js
+++ api/cheer.js
@@ -1,18 +1,1 @@
-export default function handler(request, response) {
-  // CORS ayarlarini ekliyoruz ki tarayici engellemesin
-  response.setHeader('Access-Control-Allow-Origin', '*');
-  response.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
-  response.setHeader('Content-Type', 'application/json');
 
-  // URL'den ismi cekiyoruz (?name=isim)
-  const { name } = request.query;
-  
-  // Isim girilmisse onu kullan, girilmemisse "Misafir" yaz
-  const userName = name ? name : 'Misafir';
-
-  // Yaniti JSON olarak donduruyoruz
-  return response.status(200).json({
-    mesaj: `Hello ${userName}`,
-    durum: "Basarili"
-  });
-}
